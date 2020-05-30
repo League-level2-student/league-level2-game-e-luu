f2361bdd0027fa5ec3a82b9d5fe6a0786536de8d
e-luu <e.luu141@gmail.com>
2020-05-30 14:25:51 -0700

    some stuff

--- src/pipes.java
+++ src/pipes.java
@@ -1,4 +1,21 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
 
-public class pipes {
+public class pipes extends gameobject {
+ 
+	pipes(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		speed = 1;
+	}
 
+	void update() {
+		x = (x -= speed);
+	}
+
+	void draw(Graphics g) {
+		g.setColor(Color.GREEN);
+		g.fillRect(500, 750, width, new Random().nextInt(675)+50);
+		g.fillRect(500, 0, width, new Random().nextInt(675)+50);
+	}
 }
